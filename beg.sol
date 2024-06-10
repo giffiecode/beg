b44826510adecfbe04e5328027b478c423366d0d
Nico Pei <ypei23@cmc.edu>
2024-06-09 23:52:00 -0400

    g

--- beg.sol
+++ beg.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT 
 pragma solidity ^0.8.0; 
 
-// todo: finish game  
+// todo: finish game, final push   
 
 contract Player_C { 
     constructor(uint256 _fee, uint256 _balance) {
