05c9322691e2a363dd13fc45a0c3704b4f7be691
giffiecode <ypei23@cmc.edu>
2024-08-14 16:11:38 -0400

    dirt version

--- contract/begdirt.sol
+++ contract/begdirt.sol
@@ -1,5 +1,18 @@
 // SPDX-License-Identifier: MIT 
-pragma solidity ^0.8.0; 
+pragma solidity ^0.8.0;  
+
+
+// done: 
+// hash function: target, amount, salt  
+// commit: need to specify which address to dirt  
+// reveal: need to reveal which address to dirt 
+// elimination: based on who has more dirt, not who bids the lowest    
+
+// start game can be called multiple times 
+// refactor: don't need to get index again. store index from the getHighest function   
+// refactor: delete player bid related things 
+ 
+
 
 contract Player_C { 
     constructor(uint256 _fee, uint256 _balance) {
@@ -15,7 +28,7 @@ contract Player_C {
     struct player {
         bool active;
         uint balance; 
-        uint dirt;
+        uint dirt; 
     }
 
     // full lists of player 
@@ -25,7 +38,7 @@ contract Player_C {
     receive() external payable {}
     
 
-    // only owner can update registrationFee and startingBalance 
+    // only owner of this contract can update registrationFee and startingBalance 
     address public owner; 
     uint256 public registrationFee;  
     uint256 public startingBalance; 
@@ -77,7 +90,6 @@ contract Player_C {
         players_addresses.push(msg.sender);
         // add new player's status to the list of players 
         player_status.push(player(true, startingBalance, 0)); 
-
     }
 
     // Function to check the balance of the contract
@@ -92,16 +104,17 @@ contract Player_C {
     }
 } 
 
+
 contract Game is Player_C {
     uint256 public cycleEndTime; 
     uint256 public revealEndTime;
     uint256 public cycleDuration;
     mapping(address => uint256) public committedHashes; 
-    mapping(address => uint256) public playerBids;
-    address[] public loser;  
+    uint256[] public loser; // index of loser's address / status position   
     uint256 public left;        // num of players left 
     uint256 public finalNumPlayer;  // parameter: num of people remaining to end the game. half of participants 
-    uint256 public sumOfBalance; // total remaining balance of all remaining players when the game ends   
+    uint256 public sumOfBalance; // total remaining balance of all remaining players when the game ends    
+    bool public unstarted = true;
 
 
     // event - indexed? 
@@ -116,16 +129,16 @@ contract Game is Player_C {
 
 
     // suppose to be internal 
-    function startGame() public { 
+    function startGame() public onlyOwner{   
+        require(unstarted, "can't start game again");
         setCycleEndTime();  
         setRevealEndTime(); 
         openForJoin = false;
         setFinalNumPlayer(); 
-        left = players_addresses.length;  
-        for (uint i = 0; i < players_addresses.length; i++) { 
-            playerBids[players_addresses[i]] = 0;
-        }
-    }
+        left = players_addresses.length; 
+        unstarted = false; 
+    } 
+
 
     function setFinalNumPlayer() internal {
         finalNumPlayer = uint(players_addresses.length / 2);
@@ -177,36 +190,36 @@ contract Game is Player_C {
         }
     }
 
-
     // ethers.solidityPackedKeccak256(["uint256", "uint256"], [bid, salt]);  
     function placeBid(uint256 _hash) public current  {
         uint index = getIndex(msg.sender); 
         require(player_status[index].active = true);   
         require(committedHashes[msg.sender] == 0, "hashes already committed. can't re-commit"); 
-        require(playerBids[msg.sender] == 0, "bids already submitted. can't rebid.");
         committedHashes[msg.sender] = _hash; 
         emit CommitBid(msg.sender, _hash); 
     } 
 
-    function revealBid(uint256 _bid, uint256 _salt) public revealing {
+    function revealBid(uint256 _bid, address _target, uint256 _salt) public revealing {
         // if player fail to reveal, assume bid 0 
         // if player is eliminated, elimination check active status and ignore their 0 
-        uint index = getIndex(msg.sender); 
-        require(player_status[index].balance >= _bid);  
-        require(playerBids[msg.sender] == 0, "bids already submitted. can't rebid.");    
-        require(committedHashes[msg.sender] == uint256(keccak256(abi.encodePacked(_bid, _salt))),
+        uint index_from = getIndex(msg.sender);  
+        uint index_to = getIndex(_target); 
+        require(player_status[index_from].balance >= _bid);  
+        require(committedHashes[msg.sender] == uint256(keccak256(abi.encodePacked(_bid, _target, _salt))),
             "Not Revealed: verification failed"); 
-        playerBids[msg.sender] = _bid;  
-        player_status[index].balance -= _bid; 
+        // playerBids[msg.sender] = _bid;   
+        player_status[index_to].dirt += _bid;
+        player_status[index_from].balance -= _bid; 
         emit RevealBid(msg.sender, _bid, _salt);  
     }
     
     // omittable 
     // for testing purpose only      
-    function hash(uint256 _bid, uint256 _salt) public pure returns(uint256) {
-        return uint256(keccak256(abi.encodePacked(_bid, _salt))); 
+    function hash(uint256 _bid, address _target, uint256 _salt) public pure returns(uint256) {
+        return uint256(keccak256(abi.encodePacked(_bid, _target, _salt))); 
     } 
 
+    // get index of a wallet address 
     function getIndex(address _address) view internal returns (uint) {
         for (uint i = 0; i < players_addresses.length; i++) {
             if (players_addresses[i] == _address) {
@@ -216,46 +229,23 @@ contract Game is Player_C {
         revert("wallet not in the participants list"); 
     }
 
-    // storage memory calldata event ?? 
-    function getIndexList(address[] memory _address) view internal returns (uint[] memory) { 
-        // bool inclusion = false;  
-        uint[] memory tempResult = new uint[](_address.length);
-        uint count = 0;
-
-        for (uint i = 0; i < _address.length; i++) {
-            for (uint j = 0; j < players_addresses.length; j++) {
-                if (players_addresses[j] == _address[i]) {
-                    tempResult[count] = j;
-                    count++;
-                    break; // Optional: Break if each address in _address is unique
-                }
-            }
-        }
-
-        // Create a result array with the exact size of the number of matches found
-        uint[] memory result = new uint[](count);
-        for (uint k = 0; k < count; k++) {
-            result[k] = tempResult[k];
-        }
-        return result;
-    } 
-
-    function getLowestBidder() internal {
-        address lowestBidder = players_addresses[0];
-        uint256 lowestBid = playerBids[lowestBidder]; 
+    function getHighestDirt() internal {
+        uint256 highestDirter_index = 0;
+        uint256  highest = player_status[highestDirter_index].dirt; 
         for (uint i = 0; i < players_addresses.length; i++) {
             if (player_status[i].active == true) {
-                if (playerBids[players_addresses[i]] < lowestBid) {
-                    lowestBid = playerBids[players_addresses[i]]; 
+                if (player_status[i].dirt > highest) {
+                    highest = player_status[i].dirt; 
                     delete loser; 
-                    loser.push(players_addresses[i]);  
+                    loser.push(i);  
                 }
-                else if (playerBids[players_addresses[i]] == lowestBid) {
-                    loser.push(players_addresses[i]); 
+                else if (player_status[i].dirt == highest) {
+                    loser.push(i); 
                 }
             }
         }
     }
+ 
 
     modifier cycleEnds() {
         require(block.timestamp > revealEndTime); 
@@ -264,11 +254,10 @@ contract Game is Player_C {
 
 
     function elimination() public cycleEnds { 
-        getLowestBidder(); 
-        uint[] memory index = getIndexList(loser); // = getIndex(loser);   
-        for (uint i = 0; i < index.length; i++) {
-            player_status[index[i]].active = false;  
-            player_status[index[i]].balance = 0;
+        getHighestDirt(); 
+        for (uint i = 0; i < loser.length; i++) {
+            player_status[i].active = false;  
+            player_status[i].balance = 0;  
         }
         getActiveNum(); 
         if (left > finalNumPlayer) {
@@ -297,9 +286,6 @@ contract Game is Player_C {
 
     function reset() internal cont {
         delete loser; 
-        for (uint i = 0; i < players_addresses.length; i++) { 
-            delete playerBids[players_addresses[i]];
-        }
         setCycleEndTime();   
         setRevealEndTime();  
     } 
@@ -350,21 +336,6 @@ contract Game is Player_C {
         return count;
     }
 
-    // omitable 
-    // Function to get all player bids
-    function getAllBids() external view returns (address[] memory, uint256[] memory) {
-        uint256 length = players_addresses.length;
-        address[] memory addresses = new address[](length);
-        uint256[] memory bids = new uint256[](length);
-
-        for (uint256 i = 0; i < length; i++) {
-            addresses[i] = players_addresses[i];
-            bids[i] = playerBids[addresses[i]];
-        }
-        return (addresses, bids);
-    }
-    
-    // omitable 
     function getAllHashes() external view returns (address[] memory, uint256[] memory) {
         uint256 length = players_addresses.length;
         address[] memory addresses = new address[](length);
@@ -386,3 +357,4 @@ contract Game is Player_C {
         }
     }
 }
+
