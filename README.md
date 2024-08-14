bb9dc38c8f230c660aabe596360da72b67cdd3f3
giffiecode <ypei23@cmc.edu>
2024-08-14 16:18:37 -0400

    rules

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 BID ELIMINATION GAME
+
+regular: each round, lowest bidder(s) is eliminated 
+dirt: each round, everyone specifies how much dirt to throw at a person privately. people with the most dirt are eliminated. 
+
+
--- interface/src/index.js
+++ interface/src/index.js
@@ -7,13 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello /> 
+    <App />
   </React.StrictMode>
-); 
-
-function Hello() {
-  return <h1>hello, world!</h1>;
-}
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
